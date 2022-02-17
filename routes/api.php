55c1d158a1564911e81e2699a100d1abc400a7c3
Andrey work <veyukov.andrey@gmail.com>
2022-02-17 17:17:33 +0200

    Закрытие тоута за авторизацией

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DragController;
 use App\Http\Controllers\SubstanceController;
 use App\Http\Controllers\ManufacturerController;
+use Illuminate\Support\Facades\Artisan;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -29,26 +31,17 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 
-use Illuminate\Support\Facades\Artisan;
-Route::post('init_db_fake_data', function() {
-    Artisan::call('db:seed');
-    return "init done";
-});
 
-
-
-
-Route::get('substance/show', [ManufacturerController::class, 'index']);
-Route::get('manufacturer/show', [SubstanceController::class, 'index']);
-Route::get('drag/show', [DragController::class, 'index']);
-Route::get('drag/show/{drag}', [DragController::class, 'show']);
-Route::post('drag/add', [DragController::class, 'store']);
-Route::put('drag/update/{drag}', [DragController::class, 'update']);
-Route::delete('drag/delete/{drag}', [DragController::class, 'delete']);
-
-
-
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => 'auth:sanctum'], function() {
+    Route::get('substance/show', [ManufacturerController::class, 'index']);
+    Route::get('manufacturer/show', [SubstanceController::class, 'index']);
+    Route::get('drag/show', [DragController::class, 'index']);
+    Route::get('drag/show/{drag}', [DragController::class, 'show']);
+    Route::post('drag/add', [DragController::class, 'store']);
+    Route::put('drag/update/{drag}', [DragController::class, 'update']);
+    Route::delete('drag/delete/{drag}', [DragController::class, 'delete']);
+    Route::post('init_db_fake_data', function() {
+        Artisan::call('db:seed');
+        return "init done";
+    });
 });
