dd6caefde83d544aa16a8ed895a4d535951716b3
Andrey work <veyukov.andrey@gmail.com>
2022-02-18 08:55:47 +0200

    Настройка для тестов

--- config/database.php
+++ config/database.php
@@ -35,12 +35,18 @@ return [
 
     'connections' => [
 
+//        'sqlite' => [
+//            'driver' => 'sqlite',
+//            'url' => env('DATABASE_URL'),
+//            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+//            'prefix' => '',
+//            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
+//        ],
+
         'sqlite' => [
             'driver' => 'sqlite',
-            'url' => env('DATABASE_URL'),
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'database' => ':memory:',
             'prefix' => '',
-            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
         'mysql' => [
